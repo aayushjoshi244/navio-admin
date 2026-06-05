9a2b2f06438019e2efdb6d38029e74dc0b6c7e14
aayushjoshi244 <aayush09345@gmail.com>
2026-06-05 23:02:55 +0530

    fixing part 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `crypto` and other Node.js modules
+      include: ['crypto'],
+      // If you need `crypto-js`, the plugin will also handle its dependencies
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   optimizeDeps: {
     include: ['crypto-js'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
   },
 });
